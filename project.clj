ff554364562e6b44867d191f6289dd2e0148dc50
Niclas Meier <mail@niclas-meier.de>
2012-06-04 10:26:38 +0200

    - updated midje

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject herolabs/apns "0.1.1"
+(defproject herolabs/apns "0.1.2"
   :description "A simple lightweight library to use with the Apple push notification service."
   :url "https://github.com/HEROLABS/herolabs-apns"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-codec "1.6"]
                  [clj-json "0.5.0"]
-                 [midje "1.3.1"]
+                 [midje "1.4.0"]
                  [org.jboss.netty/netty "3.2.7.Final"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.slf4j/slf4j-api "1.6.4"]
                  [ch.qos.logback/logback-core "1.0.0"]
                  [ch.qos.logback/logback-classic "1.0.0"]
                  ]
-  :dev-dependencies [[lein-midje "1.0.7"]]
+  :dev-dependencies [[lein-midje "1.0.10"]]
   ; :aot :all
   )
